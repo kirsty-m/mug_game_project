c04a7df053968c012d36251ff44786f3a91547dd
Steven <stevenjohnmorgan@gmail.com>
2023-08-30 11:00:25 +0100

    backendfix

---
+++ mug/src/main/java/com/muggame/mug/MugApplication.java
@@ -0,0 +1,13 @@
+package com.muggame.mug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugApplication.class, args);
+	}
+
+}
