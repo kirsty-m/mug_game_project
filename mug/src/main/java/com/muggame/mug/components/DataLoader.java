0fb6f8c5c09fd274b8d06c90be47f63b1f44a2af
Steven <stevenjohnmorgan@gmail.com>
2023-09-03 20:20:45 +0100

    updated location data in dataloader

--- mug/src/main/java/com/muggame/mug/components/DataLoader.java
+++ mug/src/main/java/com/muggame/mug/components/DataLoader.java
@@ -65,9 +65,9 @@ public class DataLoader implements ApplicationRunner {
                 "As the gentle click of the kettle resonates, the show’s theme music fills the room. " +
                         "The recollection that your mug rests within your work bag nudges you off the couch, propelling you towards the front door. With a careful unzip, the abrupt realisation hits that the mug remains abandoned at the office. " +
                         "The hour-and-a-half commute, which easily stretches to two and a half hours due to traffic, does not phase you in the slightest. Abandoning the television's glow and the boiled kettle, you venture out. Retrieving the mug is paramount.");
-        DialogueOption firstLocationOption1 = new DialogueOption(2L, "What do you want to get?", "The mug",firstLocation, null );
-        DialogueOption firstLocationOption2 = new DialogueOption(3L, "What do you want to get?", "The mug",firstLocation, null );
-        DialogueOption firstLocationOption3 = new DialogueOption(4L, "What do you want to get?", "The mug",firstLocation, null );
+        DialogueOption firstLocationOption1 = new DialogueOption(2L, "What do you want to get?", "The mug",firstLocation);
+        DialogueOption firstLocationOption2 = new DialogueOption(3L, "What do you want to get?", "The mug",firstLocation);
+        DialogueOption firstLocationOption3 = new DialogueOption(4L, "What do you want to get?", "The mug",firstLocation);
 
 
         Location secondLocation = new Location("Car",
@@ -75,9 +75,9 @@ public class DataLoader implements ApplicationRunner {
                         "These slide down the glass in muddy channels, eventually joining together to form temporary pools on the sill. " +
                         "Gradually, the city draws further and further away until its polluting light no longer stains the night sky. " +
                         "The office is not far, now. ");
-        DialogueOption secondLocationOption1 = new DialogueOption(5L, "What do you think about", "The mug",secondLocation, null );
-        DialogueOption secondLocationOption2 = new DialogueOption(6L, "What do you think about", "The mug",secondLocation, null );
-        DialogueOption secondLocationOption3 = new DialogueOption(7L, "What do you think about", "The mug",secondLocation, null );
+        DialogueOption secondLocationOption1 = new DialogueOption(5L, "What do you think about", "The mug",secondLocation);
+        DialogueOption secondLocationOption2 = new DialogueOption(6L, "What do you think about", "The mug",secondLocation);
+        DialogueOption secondLocationOption3 = new DialogueOption(7L, "What do you think about", "The mug",secondLocation);
 
 
         Location thirdLocation = new Location("Office Car Park", "After a considerable spell of driving, your journey reaches its conclusion as you pull into the office parking area. " +
@@ -88,11 +88,11 @@ public class DataLoader implements ApplicationRunner {
                 "Though resembling Jim, the security guard, there is an almost caricatured distortion within his features, rendering him a haunting parody of his usual self. " +
                 "“A bit early for work, are you not? Come back in the morning, once you’ve had a good night’s rest,” Jim remarks with a chilly undertone.");
         DialogueOption thirdLocationOption1 = new DialogueOption(8L, "Think about it. Do you:", "A: Articulate your precise intentions to Jim; inform him that you wish to retrieve your mug and depart promptly." +
-                " This matter need only involve the two of you.",thirdLocation, null );
+                " This matter need only involve the two of you.",thirdLocation);
         DialogueOption thirdLocationOption2 = new DialogueOption(9L, "Think about it. Do you:", "B: Falsely assert that two teenagers are vandalising the entrance to the car park." +
-                " Whilst he investigates, you will sneak into the office, using your own key.",thirdLocation, null );
+                " Whilst he investigates, you will sneak into the office, using your own key.",thirdLocation);
         DialogueOption thirdLocationOption3 = new DialogueOption(10L, "Think about it. Do you:", "C: Display the impressive expanse of your biceps, " +
-                "remarking that you’d rather not harm an ‘old man’. That’ll scare him off.",thirdLocation, null );
+                "remarking that you’d rather not harm an ‘old man’. That’ll scare him off.",thirdLocation);
 
 
     }
