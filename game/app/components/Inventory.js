a39cccb08a625cf88ccf832ab24153f5886eedf8
Kirsty <kirstmcbride@gmail.com>
2023-09-04 10:56:04 +0100

    tw elements added

--- game/app/components/Inventory.js
+++ game/app/components/Inventory.js
@@ -3,22 +3,35 @@ import React from 'react'
 export default function Inventory() {
   return (
     <>
-    <button className='button' type='button'
-    data-te-collapse-init
-    data-te-target="#inventory-box"
-    aria-expanded="false"
-    aria-controls="inventory-box">INVENTORY</button>
+    <button className='button' type="button"
+  data-te-collapse-init
+  data-te-target="#collapseWidthExample"
+  data-te-ripple-init
+  data-te-ripple-color="light"
+  aria-expanded="false"
+  aria-controls="collapseWidthExample">INVENTORY</button>
 
-    <div className='inventory-box' 
-    id='inventory-box !visible hidden'
-    data-te-collapse-item >
-        <div className='inventory-list'>
+<div
+  class="!visible hidden rounded-lg shadow-lg"
+  data-te-collapse-item
+  data-te-collapse-horizontal
+  id="collapseWidthExample">
+  <div
+    class="inventory-list">
             <ul>Item 1</ul>
             <ul>Item 2</ul>
             <ul>Item 3</ul>
+  </div>
+</div>
+
+    {/* <div className='inventory-box' 
+    id='inventory-box !visible hidden'
+    data-te-collapse-item >
+        <div className='inventory-list'>
+
         </div>
 
-    </div>
+    </div> */}
     </>
   )
 }
--- game/tailwind.config.js
+++ game/tailwind.config.js
@@ -1,3 +1,12 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js}",
+      "./node_modules/tw-elements/dist/js/**/*.js"
+    ],
+    plugins: [require("tw-elements/dist/plugin.cjs")],
+    darkMode: "class"
+  };
+
 // /** @type {import('tailwindcss').Config} */
 // module.exports = {
 //   // content: [
