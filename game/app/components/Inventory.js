5a051202d743ae8fc4571c4e888d014cc382c9f4
Kirsty <kirstmcbride@gmail.com>
2023-09-04 14:28:32 +0100

    inventory has a list

--- game/app/components/Inventory.js
+++ game/app/components/Inventory.js
@@ -3,12 +3,14 @@ import React from 'react'
 export default function Inventory() {
   return (
     <>
-    <details className='button'>
-    <summary>INVENTORY</summary>
-  <div className="collapse-content"> 
-    <p>content</p>
-  </div>
-</details>
+    <details className='inventory'>
+        <summary className='button'>INVENTORY</summary>
+        <div className="inventory-list"> 
+            <ul>Item 1</ul>
+            <ul>Item 2</ul>
+            <ul>Item 3</ul>
+        </div>
+    </details>
 
     </>
   )
