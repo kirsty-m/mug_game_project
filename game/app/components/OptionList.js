3563d54fe2bbde2a8afd10d1e63adb876333e237
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-09-05 12:01:06 +0100

    Response Text working

--- game/app/components/OptionList.js
+++ game/app/components/OptionList.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 export default function OptionList({ games }) {
-    console.log(games);
+    const [selectedOption, selectOption] = useState(null);
 
+    const handleOptionClick = (option) => {
+        selectOption(option);
+    };
 
     if (games.length > 0 && games[0].location) {
         const location = games[0].location;
-
 
         const currentOptions = location.dialogueOptions.filter((dialogueOption) => {
             return !dialogueOption.previousId;
         });
 
-
         const OptionsElements = currentOptions.map((option, index) => (
             <li key={index}>
-                <Link href="/play">{option.dialogue}</Link>
+                <button onClick={() => handleOptionClick(option)}>
+                    {option.dialogue}
+                </button>
             </li>
         ));
 
         return (
             <div className="options">
                 <ul>{OptionsElements}</ul>
+                {selectedOption && (
+                    <div className="selected-option">
+                        <p>{selectedOption.responseText}</p>
+                    </div>
+                )}
             </div>
         );
     }
-
 
     return <div>No games available.</div>;
 }
