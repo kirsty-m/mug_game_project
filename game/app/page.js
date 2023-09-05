2dc7864f3a064b310bd8fa73be7f273f259da3de
Kirsty <kirstmcbride@gmail.com>
2023-09-05 20:05:13 +0100

    animated border

--- game/app/page.js
+++ game/app/page.js
@@ -1,8 +1,31 @@
 "use client";
 import { Auth } from './components/auth'
+import Image from 'next/image';
+import mug from './static/mug.svg'
+import mugquest from './static/mugquest.svg'
 
 export default function Home() {
   return (
-    <Auth/>
+    <div className='home-page'>
+      <Auth/>
+      <div className='hero'>
+        <div className='mug-quest'>
+        <Image
+          src={mug}
+          alt='ascii mug'
+          width={500}
+          height={300}
+          priority lazy 
+        />
+        <Image
+          src={mugquest}
+          alt='ascii mug quest'
+          width={500}
+          height={200}
+          priority lazy
+        />
+        </div>
+      </div>
+    </div>
   )
 }
